c4b78a38fb99c672568558b3b0dd0519c22a4335
Nazmul_Hossain <nazmulhossain2905@gmail.com>
2024-08-29 09:30:20 +0600

    express server/library done

--- index.js
+++ index.js
@@ -3,25 +3,28 @@ const Express = require("./lib/Express");
 const app = new Express();
 const port = 3000;
 
-app.use(
-  (req, res) => {
-    console.log("Middleware using................................");
-  },
-  (err, req, res) =>
-    console.log("Another middleware using................................")
-);
+app.get(
+  "/health",
 
-app.get("/health", (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Server health is Wealth",
-    user: {
-      name: "Nazmul Hossain",
-      email: "nazmulhossain@gmail.com",
-      country: "Bangladesh",
-    },
-  });
-});
+  (req, res, next) => {
+    req.body.checking = "checking......";
+
+    // next();
+  },
+
+  (req, res) => {
+    res.status(200).json({
+      success: true,
+      message: "Server health is Wealth",
+      checking: req.body?.checking,
+      user: {
+        name: "Nazmul Hossain",
+        email: "nazmulhossain@gmail.com",
+        country: "Bangladesh",
+      },
+    });
+  }
+);
 
 app.post("/user", (req, res) => {
   console.log(req.query);
